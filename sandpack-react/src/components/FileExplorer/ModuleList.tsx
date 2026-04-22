76965fe120aa1daea1abad0104d17cfb0faf1360
Peter Neumark <neumark.peter@gmail.com>
2026-04-22 11:42:51 +0200

    get rid of circular dependency

--- sandpack-react/src/components/FileExplorer/ModuleList.tsx
+++ sandpack-react/src/components/FileExplorer/ModuleList.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 
 import type { SandpackOptions } from "../../types";
 
-import { Directory } from "./Directory";
 import { File } from "./File";
 import { fromPropsToModules } from "./utils";
 
@@ -17,6 +16,56 @@ export interface ModuleListProps extends SandpackFileExplorerProp {
   depth?: number;
   visibleFiles: NonNullable<SandpackOptions["visibleFiles"]>;
 }
+
+export interface DirectoryProps extends SandpackFileExplorerProp {
+  prefixedPath: string;
+  files: SandpackBundlerFiles;
+  selectFile: (path: string) => void;
+  activeFile: NonNullable<SandpackOptions["activeFile"]>;
+  depth: number;
+  visibleFiles: NonNullable<SandpackOptions["visibleFiles"]>;
+}
+
+export const Directory: React.FC<DirectoryProps> = ({
+  prefixedPath,
+  files,
+  selectFile,
+  activeFile,
+  depth,
+  autoHiddenFiles,
+  visibleFiles,
+  initialCollapsedFolder,
+}) => {
+  const [open, setOpen] = React.useState(
+    !initialCollapsedFolder?.includes(prefixedPath),
+  );
+
+  const toggle = (): void => setOpen((prev) => !prev);
+
+  return (
+    <div key={prefixedPath}>
+      <File
+        depth={depth}
+        isDirOpen={open}
+        onClick={toggle}
+        path={prefixedPath + "/"}
+      />
+
+      {open && (
+        <ModuleList
+          activeFile={activeFile}
+          autoHiddenFiles={autoHiddenFiles}
+          depth={depth + 1}
+          files={files}
+          initialCollapsedFolder={initialCollapsedFolder}
+          prefixedPath={prefixedPath}
+          selectFile={selectFile}
+          visibleFiles={visibleFiles}
+        />
+      )}
+    </div>
+  );
+};
 
 export const ModuleList: React.FC<ModuleListProps> = ({
   depth = 0,
